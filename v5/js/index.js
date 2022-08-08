cfaacd7a97aaf8305cdb60c200f29bff18d52230
smzord <91662040+smzord@users.noreply.github.com>
2022-08-08 12:09:17 +0530

    move code to index js
    move code to index js

--- v5/js/index.js
+++ v5/js/index.js
@@ -178,6 +178,68 @@ function Home() {
           </div>
         </div>
       </div>
+
+      <script
+      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCbP0GLeb3rSS3HB9f5N7DGgtCm7oXF_O8&callback=initAutocomplete&libraries=places&v=weekly"
+      defer
+    ></script>
+
+    <script>
+      function initAutocomplete() {
+        address1Field = document.querySelector("#address");
+        //address2Field = document.querySelector("#address2");
+        //postalField = document.querySelector("#postcode");
+
+        autocomplete = new google.maps.places.Autocomplete(address1Field, {
+          componentRestrictions: { country: ["us", "ca"] },
+          fields: ["address_components", "geometry"],
+          types: ["address"],
+        });
+        address1Field.focus();
+        autocomplete.addListener("place_changed", fillInAddress);
+      }
+
+      function fillInAddress() {
+        const place = autocomplete.getPlace();
+        let address1 = "";
+        let postcode = "";
+        for (const component of place.address_components) {
+          const componentType = component.types[0];
+
+          switch (componentType) {
+            case "street_number": {
+              address1 = `${component.long_name} ${address1}`;
+              break;
+            }
+
+            case "route": {
+              address1 += component.short_name;
+              break;
+            }
+
+            case "postal_code": {
+              postcode = `${component.long_name}${postcode}`;
+              break;
+            }
+
+            case "postal_code_suffix": {
+              postcode = `${postcode}-${component.long_name}`;
+              break;
+            }
+          }
+        }
+
+        address1Field.value = address1;
+        //postalField.value = postcode;
+        //address2Field.focus();
+      }
+      
+      
+
+        window.initAutocomplete = initAutocomplete;
+    </script>
+
+
     </div>
   );
 }
