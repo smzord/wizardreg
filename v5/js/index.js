2788c9ef01e98e663356adfc64896bda7f7a7c1b
smzord <91662040+smzord@users.noreply.github.com>
2022-08-08 11:57:09 +0530

    index html and js
    index html and js

--- v5/js/index.js
+++ v5/js/index.js
@@ -109,57 +109,7 @@ function Home() {
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
   };
   
-  const initAutocomplete = () => {
-    address1Field = document.querySelector("#address");
-    //address2Field = document.querySelector("#address2");
-    //postalField = document.querySelector("#postcode");
-    
-    autocomplete = new google.maps.places.Autocomplete(address1Field, {
-      componentRestrictions: { country: ["us", "ca"] },
-      fields: ["address_components", "geometry"],
-      types: ["address"],
-    });
-    address1Field.focus();
-    autocomplete.addListener("place_changed", fillInAddress);
-  }
-  
-  const fillInAddress = () => {
-    const place = autocomplete.getPlace();
-    let address1 = "";
-    let postcode = "";
-    for (const component of place.address_components) {
-      const componentType = component.types[0];
-  
-      switch (componentType) {
-        case "street_number": {
-          address1 = `${component.long_name} ${address1}`;
-          break;
-        }
-        
-        case "route": {
-          address1 += component.short_name;
-          break;
-        }
-  
-        case "postal_code": {
-          postcode = `${component.long_name}${postcode}`;
-          break;
-        }
-  
-        case "postal_code_suffix": {
-          postcode = `${postcode}-${component.long_name}`;
-          break;
-        }
-      }
-    }
-      
-    address1Field.value = address1;
-    //postalField.value = postcode;
-    //address2Field.focus();
-  }
-  
   useEffect(() => {
-    window.initAutocomplete = initAutocomplete;
     
     setTimeout(()=>{
       env = getCookie('env');
